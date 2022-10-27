bfb40b721e37821a7f7d902abd666253a2c48d20
Eunsong Park <run.eunsong@gmail.com>
2022-10-27 22:11:08 +0900

    2차 최종

---
+++ routes/orderRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { orderController } = require('../controllers'); 
+const { loginRequired } = require('../utils/auth');
+
+const router = express.Router();
+
+router.get('', loginRequired, orderController.orderInfo);
+router.post('', loginRequired, orderController.orderAdd);
+module.exports = router;
--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const productService = require('./productService');
 const moveProductService = require('./moveProductService');
-const userService = require('./userService')
+const userService = require('./userService');
+const orderService = require('./orderService');
 
 module.exports = { 
 	userService,
 	moveProductService,
-	productService
+	productService,
+	orderService
 }
